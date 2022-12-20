3e0ca8759635067c6d7f74dc55ddc1b043e3c3eb
Arunkumar <arunkumaryargoal@gmail.com>
2022-12-20 13:37:49 +0530

    commit

--- git_ShoppingMallManagementSystem/src/test/java/com/tnsif/client/ShoppingMallManagementSystemApplicationTests.java
+++ git_ShoppingMallManagementSystem/src/test/java/com/tnsif/client/ShoppingMallManagementSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.tnsif.client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {EmployeeRepositoryTests.class})
+@SpringBootTest
 class ShoppingMallManagementSystemApplicationTests {
 
 	@Test
